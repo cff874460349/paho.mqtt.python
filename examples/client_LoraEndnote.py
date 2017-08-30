9f8898592a098a4655aabda0c8e19e925f63eaa3
cff874460349 <874460349@qq.com>
2017-08-30 21:53:50 +0800

    update

--- examples/client_LoraEndnote.py
+++ examples/client_LoraEndnote.py
@@ -66,8 +66,8 @@ if __name__ == '__main__':
   
 
     try:
-        a=1
-        a=mqttc.connect("172.18.33.194", port=1883, keepalive=60)
+        a=100
+        a=mqttc.connect("172.18.33.", port=1883, keepalive=60)
         print("a=%d" %a)
         mqttc.subscribe("0CB16D62C9FB5828/devices/0000000022000003/up", 0)   
         mqttc.loop_forever()
