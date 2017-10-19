38d38132e0844383d97fe1e4d34aaf33b9ba0e1e
cff874460349 <874460349@qq.com>
2017-10-19 15:55:20 +0800

    import paho

--- tests/test_iot.py
+++ tests/test_iot.py
@@ -13,6 +13,32 @@ from collections import OrderedDict
 import pytest
 from enum import Enum
 
+try:
+    import paho
+
+except ImportError:
+    # This part is only required to run the examples from within the examples
+    # directory when the module itself is not installed.
+    import sys
+    import os
+    import inspect
+
+    cmd_subfolder = os.path.realpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(
+                    inspect.getfile(inspect.currentframe())
+                )[0],
+                "..",
+                "src"
+            )
+        )
+    )
+    if cmd_subfolder not in sys.path:
+        sys.path.insert(0, cmd_subfolder)
+
+    import paho
+
 
 class deviceType(Enum):
     IRT3122 = 1
